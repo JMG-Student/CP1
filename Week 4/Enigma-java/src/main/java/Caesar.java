5a428dc543091003d59d735a83817baba73ed345
Rachael Kenny <157695390+crn3@users.noreply.github.com>
2024-02-17 15:05:39 +0000

    add code to account for non-characters

--- Week 4/Enigma-java/src/main/java/Caesar.java
+++ Week 4/Enigma-java/src/main/java/Caesar.java
@@ -3,7 +3,7 @@ public class Caesar{
 public static void main(String[] args)
 {
       Caesar caesar = new Caesar();
-      System.out.print(caesar.ceasarShift("wyz", -1)); //test
+      System.out.print(caesar.ceasarShift("can80k", 7)); //test
 }
    
    public String ceasarShift(String message, int shift)
@@ -14,19 +14,24 @@ public static void main(String[] args)
         for(int i = 0; i < message.length(); i++) //looping through the message
         {
          char currentChar = message.toUpperCase().charAt(i); //getting the char at a given index
-         int alphaIndex = alphabet.indexOf(currentChar);  //getting the position of that char in the alphabet
-         
-         alphaIndex = (alphaIndex + shift) % 25; //if shift > 25, it loops around the alphabet
+         int alphaIndex = alphabet.indexOf(currentChar);  //getting the position of that char in the alphabet 
+
+         if(alphaIndex == -1)
+         {
+            newString.append(currentChar); //if char is not in alphabet just add it to final string without shifting
+         }
+         else 
+         {
+         alphaIndex = (alphaIndex + shift) % 25; //if shift > 25 making it out of bounds, it loops around the alphabet
          if(alphaIndex<0)
          {
-            alphaIndex += 25;
-         } //accounts for alphaIndex being negative, loops it back around
+            alphaIndex += 25; //accounts for alphaIndex being negative, loops it back around
+         } 
          char newChar = alphabet.charAt(alphaIndex); //new char after shifting
          newString.append(newChar);
+      }
          shift++;
          }
          return newString.toString();
-
-         //haven't accounted for spaces or non characters yet
 }
 }
